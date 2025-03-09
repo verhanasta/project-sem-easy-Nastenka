4b7822251557b66d7a8a060555408858b9300e01
a.verkholaz <verhanasta@yandex.ru>
2025-03-10 02:11:36 +0300

    test

--- internal/db/db.go
+++ internal/db/db.go
@@ -65,7 +65,7 @@ func InitDB() error {
 		CREATE TABLE IF NOT EXISTS prices (
 			id SERIAL PRIMARY KEY,
 			name TEXT NOT NULL,
-			category TEXT NOT NULL
+			category TEXT NOT NULL,
 			price NUMERIC(10,2) NOT NULL,
 			create_date TIMESTAMP NOT NULL
 		)

fb8df044fba28bb6e95fed2fafaefc5305094a97
a.verkholaz <verhanasta@yandex.ru>
2025-03-10 02:08:51 +0300

    test

--- internal/db/db.go
+++ internal/db/db.go
@@ -64,10 +64,10 @@ func InitDB() error {
 	_, err = db.Exec(`
 		CREATE TABLE IF NOT EXISTS prices (
 			id SERIAL PRIMARY KEY,
-			name TEXT,
-			category TEXT,
-			price NUMERIC(10,2),
-			create_date TIMESTAMP
+			name TEXT NOT NULL,
+			category TEXT NOT NULL
+			price NUMERIC(10,2) NOT NULL,
+			create_date TIMESTAMP NOT NULL
 		)
 	`)
 	if err != nil {

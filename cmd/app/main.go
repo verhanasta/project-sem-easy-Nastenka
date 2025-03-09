a58c1814fdddfe31f2b6464fb28d85f7a2969b0d
a.verkholaz <verhanasta@yandex.ru>
2025-03-10 01:50:45 +0300

    test

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -18,10 +18,10 @@ func main() {
 }
 
 func run() error {
-	if err := bd.InitDB(); err != nil {
+	if err := db.InitDB(); err != nil {
 		return err
 	}
-	defer bd.CloseDB()
+	defer db.CloseDB()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/api/v0/prices", handlers.HandlerPostPrices()).Methods("POST")
